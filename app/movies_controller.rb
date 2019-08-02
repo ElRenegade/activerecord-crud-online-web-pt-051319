62e6d5b1132de58672f09e13358f9c655dc8c47b
Santiago Sanchez <chagostwo@yahoo.com>
2019-08-02 02:57:06 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -29,7 +29,7 @@ def can_be_created_in_a_block(args = nil)
   # release_date == 1990
 
   Movie.create do |m|
-    m.string :title, default: 'Home Alone'
+    m.string :title, default: true, 'Home Alone'
     m.title = 'The Room'
     m.release_date = 2003
   end
