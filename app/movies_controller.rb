025f01c638b3e808f6a6cccea2f71ae2451beefa
Santiago Sanchez <chagostwo@yahoo.com>
2019-08-02 02:48:22 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -27,7 +27,7 @@ def can_be_created_in_a_block(args = nil)
   # If no arguments are passed, use default values:
   # title == "Home Alone"
   # release_date == 1990
-  attr_with_defaults = {:title=> "Home Alone"}.merge(attributes)
+  attr_with_defaults = {:title=> "Home Alone"}.merge(args)
   Movie.create do |m|
     m.title = 'The Room'
     m.release_date = 2003
